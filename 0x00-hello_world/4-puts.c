00361f4e914f0f21fc13c90d9fde75e57a6f2a1f
user_name <project@gmail.com>
2023-02-16 12:05:21 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - program to print with puts function instead printf
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
