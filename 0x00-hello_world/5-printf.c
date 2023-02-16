3f3d1165b0090b424bfda1dbc1453618fc7dac47
user_name <project@gmail.com>
2023-02-16 12:22:12 -0800

    5-printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,8 @@
  *  main - A program that print using printf
  *  Return: 0
  */
-
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
+printf("with proper grammar, but the outcome is a piece of art,\n")
 		return (0);
 }
