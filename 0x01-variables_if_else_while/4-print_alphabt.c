7371f486d88b50ca0e9ab05b33a0c5c6b7e91451
user_name <project@gmail.com>
2023-02-20 11:25:09 -0800

    Task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,22 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ * Description: print lower case alphabets.
+ * Return: Always 0
+ */
+int main(void)
+{
+	int A = 98;
+
+	while (A <= 122)
+	{
+		if (A == 101 || A == 113)
+		{
+		continue;
+		A++;
+		}
+		putchar(A);
+	}
+	putchar('\n');
+	return (0);
+}
