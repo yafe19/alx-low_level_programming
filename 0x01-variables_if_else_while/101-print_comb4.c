b2154b8281750178c9b0207a71723fc9c0ef63b4
user_name <project@gmail.com>
2023-02-20 15:09:42 -0800

    Task 101

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,44 +1,22 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Description: print all possible combination of 3 digit.
- * Retun: Always 0 (success)
+ * Description: print all combination of single number.
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	int n = 0;
-	int f_d;
-	int l_d;
+	int n;
 
-	int n2;
-	int f_d2;
-	int l_d2;
-
-	while (n <= 98)
+	for (n = 48; n <= 57; n++)
 	{
-		f_d = (n / 10 + '0');
-		l_d = (n % 10 + '0');
-		n2 = 0;
-		while (n2 <= 99)
+		putchar(n);
+		if (n == 57)
 		{
-			f_d2 = (n2 / 10 + '0');
-			l_d2 = (n2 % 10 + '0');
-
-			if (n < n2)
-			{
-				putchar(f_d);
-				putchar(l_d);
-				putchar(' ');
-				putchar(f_d2);
-				putchar(l_d2);
-				if (n != 98)
-				{
-					putchar(',');
-				}
-			}
-			n2++;
+			continue;
 		}
-		n++;
+		putchar(',');
+		putchar(' ');
 	}
 	putchar('\n');
 	return (0);
