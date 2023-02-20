b91b8811d61590dc537c0560557bf3320786fd72
user_name <project@gmail.com>
2023-02-20 15:43:45 -0800

    Task 102

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: print all combination of single number.
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++);
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
