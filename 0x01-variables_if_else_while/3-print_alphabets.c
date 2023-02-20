016da5b581e7ba6a3aa69fd5e4ba6f3a5a92e657
user_name <project@gmail.com>
2023-02-20 10:25:52 -0800

    Task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Print the alphabet
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int A = 97;
+	int a = 65;
 
+	while (A <= 122)
+	{
+		putchar(A);
+		A++;
+	}
+	while (a <= 98)
+	{
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
