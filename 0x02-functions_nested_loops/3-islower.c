bff5767e47096f485c3eaf9f8e387d86d53c2ef6
user_name <project@gmail.com>
2023-02-21 09:31:01 -0800

    4-isalpha.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _is lower - checks for a character is lowercase.
+ * _is lower - checks for a character is lowercase or not.
  * @c: the character
  * Return: 1 if letter is lowercase, 0 if not
  */
