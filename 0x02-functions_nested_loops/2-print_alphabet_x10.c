e8dd5dcbf21dd49d9a15fffd68de21bbbc5ccd20
user_name <project@gmail.com>
2023-02-21 07:35:28 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,9 +6,13 @@
 
 void print_alphabet_x10(void)
 {
+	int l_10;
 	char letter;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
-	_putchar('\n');
+	for (l_10 = 0; l_10 <= 9; l_10++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		_putchar('\n');
+	}
 }
