17522cc060b3c1bc719a925c972d2c7943ea125c
user_name <project@gmail.com>
2023-02-22 07:31:47 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-main.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *  main - check the code
+ *
+ *  Return: Always 0.
+ */
+
+int main(void)
+{
+	int r;
+
+	r = abs(-1);
+	printf("%d\n", r);
+	r = abs(0);
+	printf("%d\n", r);
+	r = abs(1);
+	printf("%d\n", r);
+	r = abs(-98);
+	printf("%d\n", r);
+	return (0);
+}
