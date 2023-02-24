e1fc3197a7041890c73e0754d2b84d6bd0e57ddb
user_name <project@gmail.com>
2023-02-24 13:02:35 -0800

    C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * _isupper - uppercase letters
- * @c: char to check
- * Return: 0 or 1.
+ * _isupper - uppercase letters.
+ * @c: char to be check
+ *
+ * Return: 0 or 1
  */
 
 int _isupper(int c)
