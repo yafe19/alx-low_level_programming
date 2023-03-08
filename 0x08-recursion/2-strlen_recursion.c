24bfe75434442f9768eb7bc170df574559ad2234
user_name <project@gmail.com>
2023-03-08 11:25:11 -0800

    Task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,11 +8,12 @@
  */
 int _strlen_recursion(char *s)
 {
-	int len = 0;
+	/*Base condition*/
 
-	if (*s)
-	{
-		len++;
-		len += _strlen_recursion(s + 1);
-	}
+	if (*s == '\0')
+		return (0);
+
+	else
+
+		return (1 + _strlen_recursion(s + 1)); /*sum 1*/
 }
