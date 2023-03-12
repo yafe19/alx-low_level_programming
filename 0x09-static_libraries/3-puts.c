1160199c5710e746ded4198cc18df28283a85a8c
user_name <project@gmail.com>
2023-03-12 08:16:46 -0700

    Task 1

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * _puts - Entry point
+ * @s: input
+ *
+ * Return: 0
+ */
+
+void _puts(char *s)
+{
+	puts(s);
+}
