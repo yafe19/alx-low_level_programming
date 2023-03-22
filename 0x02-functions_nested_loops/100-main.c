365ddcabdc1fbcdb7ec0eb1eadbb30e26b94ebca
user_name <project@gmail.com>
2023-03-21 23:54:02 -0700

    Task 1000

---
+++ 0x02-functions_nested_loops/100-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_time_table(3);
+	_putchar('\n');
+	print_time_table(5);
+	_putchar('\n');
+	print_time_table(98);
+	_putchar('\n');
+	print_time_table(12);
+	return (0);
+}
